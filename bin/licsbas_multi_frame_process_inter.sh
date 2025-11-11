9641bb0a40767bc404a5efde333f457d0e26022f
[mnergizci] <eemne@leeds.ac.uk>
2025-11-11 16:36:56 +0000

    updates

--- bin/licsbas_multi_frame_process_inter.sh
+++ bin/licsbas_multi_frame_process_inter.sh
@@ -95,30 +95,30 @@ while read -r i; do
             if [[ "$abs_flag" == "yes" ]]; then
                 sbatch --qos=high --output=preprocess/preprocessing_jasmin${i}.out --error=preprocess/preprocessing_jasmin${i}.err \
                 --job-name=prepros_LB_${i} -n 8 --time=47:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard \
-                --wrap="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -E 6 -p -X -x ${i} ${start_date} ${end_date}"
+                --wrap="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -E 6 -p -X -x ${i} ${start_date} ${end_date}"
             else
                 sbatch --qos=high --output=preprocess/preprocessing_jasmin${i}.out --error=preprocess/preprocessing_jasmin${i}.err \
                 --job-name=prepros_LB_${i} -n 8 --time=47:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard \
-                --wrap="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -E 6 -p -X ${i} ${start_date} ${end_date}"
+                --wrap="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -E 6 -p -X ${i} ${start_date} ${end_date}"
             fi
         elif [[ "$sboi_flag" == "yes" ]]; then
             if [[ "$abs_flag" == "yes" ]]; then
                 sbatch --qos=high --output=preprocess/preprocessing_jasmin${i}.out --error=preprocess/preprocessing_jasmin${i}.err \
                 --job-name=prepros_LB_${i} -n 8 --time=47:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard \
-                --wrap="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -p -X -x ${i} ${start_date} ${end_date}"
+                --wrap="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -p -X -x ${i} ${start_date} ${end_date}"
             else
                 sbatch --qos=high --output=preprocess/preprocessing_jasmin${i}.out --error=preprocess/preprocessing_jasmin${i}.err \
                 --job-name=prepros_LB_${i} -n 8 --time=47:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard \
-                --wrap="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -p -X ${i} ${start_date} ${end_date}"
+                --wrap="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -p -X ${i} ${start_date} ${end_date}"
             fi
         elif [[ "$eqoff_flag" == "yes" ]]; then
             sbatch --qos=high --output=preprocess/preprocessing_jasmin${i}.out --error=preprocess/preprocessing_jasmin${i}.err \
             --job-name=prepros_LB_${i} -n 8 --time=47:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard \
-            --wrap="licsar2licsbas.sh -M 10 -Z -g -n 8 -W -N -T -i -e -u -t 0.2 -d -E 6 -p -O -X ${i} ${start_date} ${end_date}"
+            --wrap="licsar2licsbas.sh -M 10 -g -n 8 -W -N -T -i -e -u -t 0.2 -d -E 6 -p -O -X ${i} ${start_date} ${end_date}"
         else
             sbatch --qos=high --output=preprocess/preprocessing_jasmin${i}.out --error=preprocess/preprocessing_jasmin${i}.err \
             --job-name=prepros_LB_${i} -n 8 --time=47:59:00 --mem=65536 -p comet --account=comet_lics --partition=standard \
-            --wrap="licsar2licsbas.sh -M 10 -Z -g -n 8 -W -N -T -i -e -u -t 0.2 -d -p -O -X ${i} ${start_date} ${end_date}"
+            --wrap="licsar2licsbas.sh -M 10 -g -n 8 -W -N -T -i -e -u -t 0.2 -d -p -O -X ${i} ${start_date} ${end_date}"
         fi
     else
         # Run using tmux
@@ -130,22 +130,22 @@ while read -r i; do
         ###here is second step to run the script after iono and set correction is already calculated and saved in epochs
         if [[ "$sboi_flag" == "yes" && "$eqoff_flag" == "yes" ]]; then
             if [[ "$abs_flag" == "yes" ]]; then
-                tmux_command+="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -E 6 -p -x '$i' '$start_date' '$end_date'"
+                tmux_command+="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -E 6 -p -x '$i' '$start_date' '$end_date'"
             else 
-                tmux_command+="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -E 6 -p '$i' '$start_date' '$end_date'"
+                tmux_command+="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -E 6 -p '$i' '$start_date' '$end_date'"
             fi
 
         elif [[ "$sboi_flag" == "yes" ]]; then
             if [[ "$abs_flag" == "yes" ]]; then
-                tmux_command+="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -p -x '$i' '$start_date' '$end_date'"
+                tmux_command+="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -p -x '$i' '$start_date' '$end_date'"
             else
-                tmux_command+="licsar2licsbas.sh -W -M 10 -Z -b -Q -n 8 -T -i -e -O -p '$i' '$start_date' '$end_date'"
+                tmux_command+="licsar2licsbas.sh -W -M 10 -b -Q -n 8 -T -i -e -O -p '$i' '$start_date' '$end_date'"
             fi
             
         elif [[ "$eqoff_flag" == "yes" ]]; then
-            tmux_command+="licsar2licsbas.sh -M 10 -Z -g -n 8 -W -N -T -i -e -u -t 0.2 -d -E 6 -p -O '$i' '$start_date' '$end_date'"
+            tmux_command+="licsar2licsbas.sh -M 10 -g -n 8 -W -N -T -i -e -u -t 0.2 -d -E 6 -p -O '$i' '$start_date' '$end_date'"
         else
-            tmux_command+="licsar2licsbas.sh -M 10 -Z -g -n 8 -W -N -T -i -e -u -t 0.2 -d -p -O '$i' '$start_date' '$end_date'"
+            tmux_command+="licsar2licsbas.sh -M 10 -g -n 8 -W -N -T -i -e -u -t 0.2 -d -p -O '$i' '$start_date' '$end_date'"
         fi
 
         tmux_command+=" >> '${log_dir}/${i}_out.log' 2>> '${log_dir}/${i}_err.log' && echo 'Job for $i completed; bash'"
